37d2d5d16bb4e10c98da23ea7a49e61fe3540a3e
anilucia <amilucia07@gmail.com>
2026-03-30 21:20:02 -0300

    Ver se o número é par ou ímpar

---
+++ 2-estruturas-de-controle/par-ímpar.py
@@ -0,0 +1,7 @@
+#4. Solicite ao usuário que informe um número e depois exiba se é par ou ímpar.
+
+numero = int(input("Digite um número: "))
+if numero % 2 == 0:
+    print("Par")
+else:
+    print("Ímpar")
