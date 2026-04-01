134ffabf74876b76a2436d48d0c0548bc650ddc9
anilucia <amilucia07@gmail.com>
2026-03-31 22:57:45 -0300

    Lista de compras

---
+++ 4-lista-tupla-dicionario/lista_compras.py
@@ -0,0 +1,17 @@
+lista_compras = []
+
+item = input("Digite um item: ")
+lista_compras.append(item)
+print(lista_compras)
+
+item = input("Digite outro item: ")
+lista_compras.append(item)
+print(lista_compras)
+
+item = input("Digite o próximo item: ")
+lista_compras.append(item)
+print(lista_compras)
+
+remover = input("Digite um item para remover: ")
+lista_compras.remove(remover)
+print(lista_compras)
