f19fc42a5a90cfb5e6555147555126b5d9243519
anilucia <amilucia07@gmail.com>
2026-03-31 23:16:51 -0300

    Aluno nome e idade

---
+++ 4-lista-tupla-dicionario/aluno.py
@@ -0,0 +1,10 @@
+nome = input("Digite o nome: ")
+idade = int(input("Digite a idade: "))
+
+aluno = {
+    "nome": nome,
+    "idade": idade
+}
+
+print("Dicionário:", aluno)
+print("Tipo:", type(aluno))
