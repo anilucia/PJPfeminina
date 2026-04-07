2bb8e64d6d6e0492309d243573e5bd208689f00f
anilucia <amilucia07@gmail.com>
2026-04-07 18:53:17 -0300

    Fix delete parametro

--- 5-funcoes/parametro.py
+++
@@ -1,8 +0,0 @@
-#Crie uma função que receba um nome como parâmetro e exiba a mensagem:'Bem-vindo, NOME'
-
-def boas_vindas(nome):
-    print("Bem-vindo,", nome)
-nome = input("Digite seu nome: ")
-boas_vindas(nome)
-    
-    
