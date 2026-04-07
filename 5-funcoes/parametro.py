6a3e29adf8f13b2adf78a7ac2b9c7a9e28b83458
anilucia <amilucia07@gmail.com>
2026-04-07 16:56:21 -0300

    Recebe nome como parâmetro

---
+++ 5-funcoes/parametro.py
@@ -0,0 +1,8 @@
+#Crie uma função que receba um nome como parâmetro e exiba a mensagem:'Bem-vindo, NOME'
+
+def boas_vindas(nome):
+    print("Bem-vindo,", nome)
+nome = input("Digite seu nome: ")
+boas_vindas(nome)
+    
+    
