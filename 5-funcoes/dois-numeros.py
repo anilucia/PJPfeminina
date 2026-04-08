995a98cbd745077cd3f0a679f26b1b3f57fd2303
anilucia <amilucia07@gmail.com>
2026-04-07 21:14:04 -0300

    Receba dois números e retorne a soma deles

---
+++ 5-funcoes/dois-numeros.py
@@ -0,0 +1,6 @@
+#Crie uma função que receba dois números e retorne a soma deles
+def soma(a, b):
+    return a + b
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+print("Soma:", soma(n1, n2))
